04a95434c71f9bfa1b886abd1d97982770814dde
BenJR546 <benjrice546@gmail.com>
2024-06-11 16:45:36 +1000

    Filled out devlog, made a barebones clicker to build my game around, styles screenshots to not take up too much width

---
+++ godot/scenes/clicker.gd
@@ -0,0 +1,21 @@
+extends Control
+
+@export var label: Label
+var valueSwords: int = 0
+
+func _ready():
+	update_label_text()
+
+func _on_button_pressed():
+	create_sword()
+
+func create_sword():
+	valueSwords += 1
+	update_label_text()
+
+func update_label_text():
+	label.text = "Swords: %s" % valueSwords
+
+
+func _on_texture_button_pressed():
+	pass # Replace with function body.
